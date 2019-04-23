04a1f6ea4559ddc5d67c132d0b8f7b8942b574cc
Airosmaa <xmaiju.airosmaa@capgemini.com>
2019-04-23 20:45:06 +0300

    little fixes

--- src/components/Observation.js
+++ src/components/Observation.js
@@ -7,6 +7,15 @@ function notesLength(notes) {
     return notes
 }
 
+function geolocation(lat, long) {
+    if (lat === "") {
+        return
+    }
+    const coords = "Latitude: " + lat + ", Longitude: " + long;
+    return coords;
+
+}
+
 const Observation = (props) => (
     <div className="row">
         <div className="col s12 m6">
@@ -14,9 +23,11 @@ const Observation = (props) => (
                 <div className="card-content">
                     <span className="card-title">{props.observation.name}</span>
                     <p className="grey-text">{props.observation.rarity}</p>
+                </div>
+                <div className="card-action">
                     <p className="grey-text">{props.observation.timestamp}</p>
-                    <p className="grey-text">Latitude: {props.observation.latitude}</p>
-                    <p className="grey-text">Longitude: {props.observation.longitude}</p>
+
+                    <p className="grey-text">{geolocation(props.observation.latitude, props.observation.longitude)}</p>
                 </div>
                 <div className="card-action">
                     <p>{notesLength(props.observation.notes)}</p>
